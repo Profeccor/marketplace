850d5a2715e5f60b01488f622e75538defd5f644
Fahrul Arifin <soraroxes46@gmail.com>
2020-06-17 13:30:53 +0700

    menambahkan middleware dan routing product

--- routes/products.js
+++ routes/products.js
@@ -2,8 +2,12 @@ const router = require('express').Router()
 
 const ProductController = require('../controllers/product')
 
-router.get('/:productID', ProductController.getData)
-router.delete('/:productID', ProductController.delete)
-router.post('/addProduct', ProductController.addProduct)
+const authentication = require(`../middlewares/checkAuthentication`)
+const authorization = require(`../middlewares/tokoAuthorization`)
+const Pauthorization = require(`../middlewares/productAuthorization`)
+
+router.get('/:productID',authentication,authorization,Pauthorization, ProductController.getData)
+router.delete('/:productID',authentication,authorization,Pauthorization, ProductController.delete)
+router.post('/addProduct',authentication,authorization,Pauthorization, ProductController.addProduct)
 
 module.exports = router
