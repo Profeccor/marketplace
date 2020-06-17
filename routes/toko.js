919153564586c6e8423dab9e3c9de2e9b138dbaf
Fahrul Arifin <soraroxes46@gmail.com>
2020-06-17 13:01:14 +0700

    menambahkan middleware dan routing toko

--- routes/toko.js
+++ routes/toko.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 
 const TokoController = require('../controllers/toko')
+const authentication = require(`../middlewares/checkAuthentication`)
+const authorization = require(`../middlewares/tokoAuthorization`)
 
 router.get('/:tokoID', TokoController.getData)
-router.delete('/:tokoID', TokoController.delete)
+router.delete('/:tokoID',authentication , authorization, TokoController.delete)
 router.post('/createToko', TokoController.createToko)
 
 module.exports = router
